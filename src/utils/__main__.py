9f5c10ea0134cb09dd5daaf3abf4f252a710f77a
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-02-25 00:42:04 -0600

    Mess around a bit w/ utils lib 'exports'

--- src/utils/__main__.py
+++ src/utils/__main__.py
@@ -1,15 +1,20 @@
 # utils namespace
 
-from parse_boolean import parse_boolean
-from env import *
+from .parse_boolean import parse_boolean
+from .env import *
 from logging import *
-from utils import *
-from AppConfig import AppConfig
+from utils.util import *
+from utils.log_impl import *
+from .AppConfig import AppConfig
 
 # ?? Attempt to relocate dotenv env object here?
 
 def main():
-    # utils exports
+    # utils exports; I have no clue as to if this is the proper way of
+    # doing this?
+    # a) I do not want an executable called utils to be produced.
+    # b) I think I should be doing this export in __init__.py instead so that
+    # I can treat this as a proper library namespace?
     return [
         # parse_boolean.py
         parse_boolean,
@@ -17,7 +22,7 @@ def main():
         get_env,
         get_env_bool,
         #set_env,
-        # logging.py
+        # log_impl.py
         message,
         crit,
         error,
