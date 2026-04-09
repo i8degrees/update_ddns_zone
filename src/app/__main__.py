24bfc5181bdd07822ce133bb8d81abd2e04d3433
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-04-09 18:38:12 -0500

    Output & logging

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -12,26 +12,25 @@ from utils import *
 import logging
 
 env = {
-    **os.environ,
+  **os.environ,
 }
+
 
 log = logging.getLogger('ddns_psupdate')
 
 app = None
 
 PROG_NAME = 'ddns_psupdate'
-PROG_VERSION = '%(prog)s 1.0.0'
-# ?? import utils for __version__ def
 PROG_VERSION = '%(prog)s ' + __version__
 DESCRIPTION = 'Update DNS upon DHCP lease'
-DEFAULT_LOG_LEVELS = ["DEBUG", "NOTICE", "INFO", "WARNING", "CRITICAL", "ERROR"]
 DEFAULT_CONFIG_FILE_PATH = "config/app.yml"
-DEFAULT_LOG_LEVEL = 'INFO'
+DEFAULT_LOG_LEVELS = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+DEFAULT_LOG_LEVEL = "WARNING" # 30
 DEBUG = False
 DEFAULT_CMD_OPTS = ["ADD", "OLD", "DEL"]
 
 parser = \
-    argparse.ArgumentParser(prog=PROG_NAME, description=DESCRIPTION)
+  argparse.ArgumentParser(prog=PROG_NAME, description=DESCRIPTION)
 parser.add_argument("CMD", help=f'Command is one of {DEFAULT_CMD_OPTS}')
 parser.add_argument("MAC_ADDR",
                     help="The 48-bit link-layer address")
@@ -42,63 +41,52 @@ parser.add_argument("HOSTNAME", type=str,
 parser.add_argument("-c", "--config", default=DEFAULT_CONFIG_FILE_PATH,
                     help=f"The file path to the configuration file")
 parser.add_argument("-d", "--debug", action="store_true",
-                    help="Enable debugging code")
+                    help="Enable extra debug code")
 parser.add_argument("-l", "--log", default=DEFAULT_LOG_LEVEL,
-    help=f'Set the log level to one of {DEFAULT_LOG_LEVELS}')
+                    help=f'Set the log level to one of {DEFAULT_LOG_LEVELS}')
 parser.add_argument("-v", "--version", action="version", version=PROG_VERSION)
 
 args = parser.parse_args()
 DEBUG = bool(args.debug)
-LOG_LEVEL = DEFAULT_LOG_LEVEL
+
+log_level = DEFAULT_LOG_LEVEL
 if args.log.upper() in DEFAULT_LOG_LEVELS:
-    LOG_LEVEL = args.log.upper()
-CMD = args.CMD.lower()
-MAC_ADDR = args.MAC_ADDR
-IP_ADDR = args.IP_ADDR
-HOSTNAME = args.HOSTNAME
+  log_level = args.log.upper()
+
+numeric_log_level = getattr(logging, log_level.upper(), None)
+
+if not isinstance(numeric_log_level, int):
+  raise ValueError('Invalid log level: %s' % log_level)
+log.setLevel(numeric_log_level)
+
 APP_CONFIG = args.config
 
 if APP_CONFIG:
-    if not (os.path.isfile(APP_CONFIG) and os.access(APP_CONFIG, os.R_OK)):
-      print(f"ERROR: The given file path at {args.config} does not exist.")
-      exit(1)
-    else:
-       print(f"Found configuration file at {APP_CONFIG}")
-       app = AppConfig(APP_CONFIG)
+  if not (os.path.isfile(APP_CONFIG) and os.access(APP_CONFIG, os.R_OK)):
+    print(f"ERROR: The given file path at {APP_CONFIG} does not exist.")
+    exit(1)
+  else:
+    print(f"Found configuration file at {APP_CONFIG}")
+    app = AppConfig(APP_CONFIG)
 
-if LOG_LEVEL == "DEBUG":
-  log.setLevel(logging.DEBUG)
-elif LOG_LEVEL == "NOTICE":
-  log.setLevel(logging.NOTICE)
-elif LOG_LEVEL == "INFO":
-  log.setLevel(logging.INFO)
-elif LOG_LEVEL == "WARNING":
-  log.setLevel(logging.WARNING)
-elif LOG_LEVEL == "CRITICAL":
-  log.setLevel(logging.CRITICAL)
-elif LOG_LEVEL == "ERROR":
-  log.setLevel(logging.ERROR)
+log.debug(f'LOG_LEVEL={log_level}')
 
-print(LOG_LEVEL)
-if DEBUG == True:
-  print("DEBUGGING is on")
-  
 config = app.dump()
 
 # !! This is ultimately left up to the end-user to decide of which they wish
 # to do; this must be set in either case.
 PDNS_API_KEY: str = ""
 if config.get("PDNS_API_KEY") != None:
-    PDNS_API_KEY = config["PDNS_API_KEY"]
+  PDNS_API_KEY = config["PDNS_API_KEY"]
 else:
-    PDNS_API_KEY = get_env("PDNS_API_KEY", "")
+  PDNS_API_KEY = get_env("PDNS_API_KEY", "")
 assert PDNS_API_KEY != "", "The PDNS_API_KEY (X-API-Key) must be initialized."
 
 PDNS_API_VERSION: str = ""
 if config.get("PDNS_API_VERSION") != None:
-    PDNS_API_VERSION = config["PDNS_API_VERSION"]
+  PDNS_API_VERSION = config["PDNS_API_VERSION"]
 else:
-    PDNS_API_VERSION = get_env("PDNS_API_VERSION", "4.8.4")
+  PDNS_API_VERSION = get_env("PDNS_API_VERSION", "4.8.4")
 assert PDNS_API_VERSION != "", "PDNS_API_VERSION must be initialized"
 
 # !! Initial default
@@ -110,15 +98,44 @@ PDNS_CHANGE_TYPE: Final[str] = "REPLACE"
 # >> SEE ALSO
 # >> 1. https://doc.powerdns.com/authoritative/http-api/zone.html#adding-a-single-record-to-a-rrset
 if PDNS_API_VERSION >= "4.9.12":
-    PDNS_CHANGE_TYPE = "EXTEND"
+  PDNS_CHANGE_TYPE = "EXTEND"
 
 # FIXME(JEFF): This should be handled in update_record or so in utils/util.py
 PDNS_API_URL: Final[str] = config["PDNS_API_PROTO"] + "://" + \
     config["PDNS_API_HOST"] \
 + ":" + str(config["PDNS_API_PORT"])
 
+#print(app.lookup_forward("iot.ha.home.arpa"))
+
 def main() -> None:
-  args = sys.argv
+  #args = sys.argv
+
+  DNSMASQ_LOG_DHCP = \
+    get_env("DNSMASQ_LOG_DHCP", False)
+  if DNSMASQ_LOG_DHCP and bool(DNSMASQ_LOG_DHCP) == True:
+    print("Verbose DHCP logging is enabled.")
+    log.setLevel(9);
+  elif not (DNSMASQ_LOG_DHCP and DNSMASQ_LOG_DHCP == False):
+   print("Verbose DHCP logging has not been requested.");
+
+  DNSMASQ_TAGS = \
+    get_env("DNSMASQ_TAGS", "")
+  if DNSMASQ_TAGS and len(DNSMASQ_TAGS) > 0:
+    printf("DNSMASQ_TAGS=%s\n", DNSMASQ_TAGS)
+
+  DNSMASQ_INTERFACE = \
+     get_env("DNSMASQ_INTERFACE", "")
+  if DNSMASQ_INTERFACE and len(DNSMASQ_INTERFACE) > 0:
+    printf("DNSMASQ_INTERFACE=%s\n", DNSMASQ_INTERFACE)
+
+  # >> This env is set to "1" (True) during "old" events and signifies to 
+  # >> us -- at the very minimum -- that we do not need to iterate through
+  # >> the main loop as there is nothing to be done.
+  DNSMASQ_DATA_MISSING = \
+     get_env("DNSMASQ_DATA_MISSING", False)
+  if DNSMASQ_DATA_MISSING and bool(DNSMASQ_DATA_MISSING) == True:
+    print("DNSMASQ_DATA_MISSING=1\nExiting...\n")
+    #sys.exit(0);
 
   # !! IMPORTANT(JEFF): DNSMASQ_DOMAIN is a required environment variable of which is
   # only passed to this script when the end-user has dnsmasq configured with
@@ -126,7 +143,8 @@ def main() -> None:
   # parameter.
   DNSMASQ_DOMAIN = \
     canonical_dns_name(get_env("DNSMASQ_DOMAIN", ""))
-
+  
+  # ?? Rename to DNSMASQ_DOMAIN_PTR
   DNSUPDATE_ZONE_PTR = \
     canonical_dns_name(get_env("DNSUPDATE_ZONE_PTR", ""))
 
@@ -134,7 +152,7 @@ def main() -> None:
   #if not get_env("DNSMASQ_DOMAIN", "") and DEBUG == True:
     #DNSMASQ_DOMAIN = canonical_dns_name("test.home.arpa")
 
-  DNSUPDATE_ZONE_PTR: Final[str|None] = None
+  # DNSUPDATE_ZONE_PTR: Final[str|None] = None
 
   # >> HACK(JEFF): This is a monkey patch for until we figure out a proper 
   # >> configuration format for this script that matches the domain with the
@@ -157,19 +175,25 @@ def main() -> None:
     DNSUPDATE_ZONE_PTR = "14.168.192.in-addr.arpa."
     #DNSUPDATE_ZONE_PTR = canonical_dns_name("14.168.192.in-addr.arpa")
   
-  log.debug(f'DNSMASQ_DOMAIN: {DNSMASQ_DOMAIN}')
-  log.debug(f'DNSUPDATE_ZONE_PTR: {DNSUPDATE_ZONE_PTR}')
+  log.debug(f'DNSMASQ_DOMAIN={DNSMASQ_DOMAIN}')
+  log.debug(f'DNSUPDATE_ZONE_PTR={DNSUPDATE_ZONE_PTR}')
   
+  # !! FIXME
+  assert DNSMASQ_DOMAIN != "None." and DNSMASQ_DOMAIN != "", "DNSMASQ_DOMAIN must be set"
+
   # !! Required argument
   # >> DNSMASQ may pass DUID made of the link layer *...* instead of
   # >> See man 8 dnsmasq
   # >> RFC XXX
-    
+  
+  CMD = args.CMD.lower()
+  MAC_ADDR = args.MAC_ADDR
+  IP_ADDR = args.IP_ADDR
+  L_HOSTNAME = short_hostname(args.HOSTNAME)
+  
   PTR = reverse_ip(IP_ADDR)
   RIP = f'{PTR}.in-addr.arpa.'
-  #RIP = f'{PTR}.{DNSUPDATE_ZONE_PTR}'
-  #L_HOSTNAME = short_hostname(str(args[4]))
-  L_HOSTNAME = short_hostname(HOSTNAME)
+  
   FQDN = canonical_dns_name(L_HOSTNAME) + canonical_dns_name(str(DNSMASQ_DOMAIN))
   log.debug(f'FQDN:{FQDN}')
   
@@ -244,20 +268,20 @@ def main() -> None:
     elif LOG_STR == "add":
       LOG_STR = "NEW"
       
-    log.debug(f'{LOG_STR}-RR_TYPE_A_REQUEST:{json.dumps(RR_TYPE_A_REQUEST)}')
+    print(f'{LOG_STR} RR_TYPE_A_REQUEST:{json.dumps(RR_TYPE_A_REQUEST)}')
     res = update_record(url = FULL_REQUEST_URL, zone = DNSMASQ_DOMAIN, api_key = PDNS_API_KEY, json_data = RR_TYPE_A_REQUEST)
     print(res.message())
     if res.status_code() != 204:
       exit(res.status_code())
     
-    log.debug(f'{LOG_STR}-RR_TYPE_TXT_REQUEST:{json.dumps(RR_TYPE_TXT_REQUEST)}')
+    print(f'{LOG_STR} RR_TYPE_TXT_REQUEST:{json.dumps(RR_TYPE_TXT_REQUEST)}')
     res = update_record(url = FULL_REQUEST_URL, zone = DNSMASQ_DOMAIN, api_key = PDNS_API_KEY, json_data = RR_TYPE_TXT_REQUEST)
     print(res.message())
     if res.status_code() != 204:
       exit(res.status_code())
     
     if DNSUPDATE_ZONE_PTR != None:
-      log.debug(f'{LOG_STR}-RR_TYPE_PTR_REQUEST:{json.dumps(RR_TYPE_PTR_REQUEST)}')
+      print(f'{LOG_STR} RR_TYPE_PTR_REQUEST:{json.dumps(RR_TYPE_PTR_REQUEST)}')
       res = update_record(url = FULL_REQUEST_URL, zone = DNSUPDATE_ZONE_PTR, api_key = PDNS_API_KEY, json_data = RR_TYPE_PTR_REQUEST)
       print(res.message())
       if res.status_code() != 204:
@@ -271,7 +295,7 @@ def main() -> None:
     RR_TYPE_A_REQUEST_DEL = RR_TYPE_A_REQUEST
     RR_TYPE_A_REQUEST_DEL["rrsets"][0]["changetype"] = "DELETE"
     RR_TYPE_A_REQUEST_DEL["rrsets"][0]["ttl"] = None
-    log.debug(f'{LOG_STR}-RR_TYPE_A_REQUEST_DEL:{json.dumps(RR_TYPE_A_REQUEST_DEL)}')
+    print(f'{LOG_STR} RR_TYPE_A_REQUEST_DEL:{json.dumps(RR_TYPE_A_REQUEST_DEL)}')
     
     res = update_record(url = FULL_REQUEST_URL, zone = DNSMASQ_DOMAIN, api_key = PDNS_API_KEY, json_data = RR_TYPE_A_REQUEST_DEL)
     print(res.status_message())
@@ -281,7 +305,7 @@ def main() -> None:
     RR_TYPE_TXT_REQUEST_DEL = RR_TYPE_TXT_REQUEST
     RR_TYPE_TXT_REQUEST_DEL["rrsets"][0]["changetype"] = "DELETE"
     RR_TYPE_TXT_REQUEST_DEL["rrsets"][0]["ttl"] = None
-    log.debug(f'{LOG_STR}-RR_TYPE_TXT_REQUEST_DEL:{json.dumps(RR_TYPE_TXT_REQUEST_DEL)}')
+    print(f'{LOG_STR} RR_TYPE_TXT_REQUEST_DEL:{json.dumps(RR_TYPE_TXT_REQUEST_DEL)}')
 
     res = update_record(url = FULL_REQUEST_URL, zone = DNSMASQ_DOMAIN, api_key = PDNS_API_KEY, json_data = RR_TYPE_TXT_REQUEST_DEL)
     print(res.status_message())
@@ -292,7 +316,7 @@ def main() -> None:
       RR_TYPE_PTR_REQUEST_DEL = RR_TYPE_PTR_REQUEST
       RR_TYPE_PTR_REQUEST_DEL["rrsets"][0]["changetype"] = "DELETE"
       RR_TYPE_PTR_REQUEST_DEL["rrsets"][0]["ttl"] = None
-      log.debug(f'{LOG_STR}-RR_TYPE_PTR_REQUEST_DEL:{json.dumps(RR_TYPE_PTR_REQUEST_DEL)}')
+      print(f'{LOG_STR} RR_TYPE_PTR_REQUEST_DEL:{json.dumps(RR_TYPE_PTR_REQUEST_DEL)}')
     
       res = update_record(url = FULL_REQUEST_URL, zone = DNSUPDATE_ZONE_PTR, api_key = PDNS_API_KEY, json_data = RR_TYPE_PTR_REQUEST_DEL)
       print(res.status_message())
