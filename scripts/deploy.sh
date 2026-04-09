324c23df2730b655ca99e6f32cb51258369608fe
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-04-09 18:36:41 -0500

    Add another template script to compile & deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,19 +8,15 @@
   #"config/app.yml"
   #"dist/update_ddns_zone-1.0.0.tar.gz"
 #)
-DEPLOY_PACKAGE_FILENAME=update_ddns_zone-1.0.0.tar.gz # package filename
 DEPLOY_PACKAGE_PATH="dist/${DEPLOY_PACKAGE_FILENAME}" # local path
-DEPLOY_PACKAGE_DEST="/tmp" # remote path
-
-DEPLOY_CONFIG="config/app.yml" # local path
-DEPLOY_CONFIG_DEST="/usr/local/etc/app.yml" # remote path
-
+DEPLOY_PACKAGE_FILENAME=update_ddns_zone-1.0.0.tar.gz # package filename
 DEPLOY_USER=root # remote user
 DEPLOY_HOST=sw2-gw.lan # remote host
 SSH_HOST_STR="${DEPLOY_USER}@${DEPLOY_HOST}"
 # TODO(JEFF): Prefer rsync?
 DEPLOY_CMD=scp # local
 DEPLOY_RUN_CMD=ssh # local
+DEPLOY_PACKAGE_DEST="/tmp" # remote path
 
 # 1a. Transfer dist package to deployment host
 if ! $DEPLOY_CMD -O "${DEPLOY_PACKAGE_PATH}" ${SSH_HOST_STR}:${DEPLOY_PACKAGE_DEST}/.; then
@@ -28,6 +24,9 @@ if ! $DEPLOY_CMD -O "${DEPLOY_PACKAGE_PATH}" ${SSH_HOST_STR}:${DEPLOY_PACKAGE_DE
   echo
   exit 1
 fi
+
+DEPLOY_CONFIG="config/app.yml" # local path
+DEPLOY_CONFIG_DEST="/usr/local/etc/app.yml" # remote path
 
 # 1b. Copy configuration file
 # >> Do not fear if this fails -- we may have the file locked with chattr +i
@@ -38,6 +37,17 @@ if ! $DEPLOY_CMD -O ${DEPLOY_CONFIG} ${SSH_HOST_STR}:${DEPLOY_CONFIG_DEST}; then
   #exit 2
 fi
 
+DEPLOY_PACKAGE_DEST=templates/openwrt/usr/lib/dnsmasq
+DEPLOY_PACKAGE_FILENAME=dhcp-script.sh
+# 1c. Copy template(s)
+#if ! "${DEPLOY_RUN_CMD}" ${SSH_HOST_STR} "${DEPLOY_PACKAGE_DEST}/${DEPLOY_PACKAGE_FILENAME}"; then
+  #echo "CRIT: Failed to transfer ${DEPLOY_PACKAGE_FILENAME} at ${SSH_HOST_STR} - halting!"
+  #echo
+  #exit 3
+#fi
+
+
+DEPLOY_PACKAGE_DEST=/tmp
 # 2. Install package on host
 if ! "${DEPLOY_RUN_CMD}" ${SSH_HOST_STR} pipx install --force "${DEPLOY_PACKAGE_DEST}/${DEPLOY_PACKAGE_FILENAME}"; then
   echo "CRIT: Failed to transfer ${DEPLOY_CONFIG} at ${SSH_HOST_STR} - halting!"
