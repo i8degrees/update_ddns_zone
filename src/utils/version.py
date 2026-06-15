4133e7a5c6037a0264679fb881e7226739b90aaf
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-06-15 15:56:35 -0500

    Fix syntax error && do not show errors in console
    - Fix format typo in printing DNSMASQ_TAGS && DNSMASQ_INTERFACE
    env vars
    
    - Fix another format typo when we encode the JSON request dicts
    in the main app process
    
    - `__gitversion__` is dependent upon not only git but also that
    the executable is being called from the project's git tree. The end-user
    should now only see the "Unkonwn" string when deploying this script
    in production use (without a git repo).

--- src/utils/version.py
+++ src/utils/version.py
@@ -10,7 +10,7 @@ def git_revision_hash() -> str:
     """Returns the full git revision hash of the current directory."""
     try:
         # Run the git command to get the output
-        commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
+        commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'], stderr=subprocess.DEVNULL).decode('utf-8').strip()
         return commit_hash
     except subprocess.CalledProcessError:
         # Handle cases where git is not installed or not in a git repo
@@ -20,7 +20,7 @@ def git_revision_short_hash() -> str:
     """Returns the short git revision hash of the current directory."""
     try:
         # Run the git command with the --short option
-        commit_hash_short = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('utf-8').strip()
+        commit_hash_short = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], stderr=subprocess.DEVNULL).decode('utf-8').strip()
         return commit_hash_short
     except subprocess.CalledProcessError:
         return "Unknown"
